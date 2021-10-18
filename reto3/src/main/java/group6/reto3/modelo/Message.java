94bb36a407e3e68830ba0eba17f500a1ced989a7
Felipe Bernal <brandon.bernal.mt@correo.usa.edu.co>
2021-10-18 18:23:10 -0500

    Archivos message realizados

--- reto3/src/main/java/group6/reto3/modelo/Message.java
+++ reto3/src/main/java/group6/reto3/modelo/Message.java
@@ -1,11 +1,68 @@
 package group6.reto3.modelo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  * Clase mensaje
  * @author Grupo 6
  */
+@Entity
+@Table(name = "message")
 public class Message {
     
-        
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer idMessage;
+    private String messageText;
+    
+    @ManyToOne
+    @JoinColumn(name="computerId")
+    @JsonIgnoreProperties({"messages", "client", "reservations"})//
+    private Computer computer;
+    
+    @ManyToOne
+    @JoinColumn(name="clientId")
+    @JsonIgnoreProperties({"messages", "client", "reservations" })//
+    private Client client;
+
+
+    public Integer getIdMessage() {
+        return idMessage;
+    }
+
+    public void setIdMessage(Integer idMessage) {
+        this.idMessage = idMessage;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Computer getComputer() {
+        return computer;
+    }
+
+    public void setComputer(Computer computer) {
+        this.computer = computer;
+    }
     
 }
--- reto3/src/main/java/group6/reto3/servicios/MessageService.java
+++ reto3/src/main/java/group6/reto3/servicios/MessageService.java
@@ -1,10 +1,32 @@
 package group6.reto3.servicios;
 
+import group6.reto3.modelo.Message;
+import group6.reto3.repositorios.MessageRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * Clase servicio mensaje
  * @author Grupo 6
  */
+@Service
 public class MessageService {
+    @Autowired
+    private MessageRepository repo;
     
+    public List<Message> getAll() {
+        return repo.getAll();
+    }
+    
+    public Message save(Message message) {
+        return repo.save(message);
+    }   
+    
+    public Message getById(int id) {
+        Optional<Message> message = repo.getById(id);
+        return message.orElse(new Message());
+    }    
        
 }
